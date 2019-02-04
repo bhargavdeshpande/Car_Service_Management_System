f5f5bb59c66542a9815efdd413b269e54dddeafc
BhargavD <bbdeshpa@ncsu.edu>
2019-02-04 16:07:24 -0500

    pushing project to GitHub

---
+++ src/module/car/service/manage/DBUtility.java
@@ -0,0 +1,56 @@
+package module.car.service.manage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.SQLException;
+
+public class DBUtility {
+
+	static final String jdbcURL = "Enter Host URL here";
+
+	public static Connection connectDB(String username, String password) throws SQLException {
+		Connection conn = null;
+		try {
+
+			// Load the driver. This creates an instance of the driver
+			// and calls the registerDriver method to make Oracle Thin
+			// driver available to clients.
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			try {
+
+				conn = DriverManager.getConnection(jdbcURL, username, password);
+
+				if (conn != null) {
+				} else {
+					System.out.println("Failed to connect");
+				}
+
+			} catch (SQLException e) {
+				System.out.println("Connection failed!");
+				e.printStackTrace();
+				throw e;
+			}
+
+		} catch (Exception e) {
+			System.out.println("Connection failed!");
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+}
---
+++ src/module/car/service/manage/Home.java
@@ -0,0 +1,37 @@
+package module.car.service.manage;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+class Home {
+	Connection connection;
+
+	public void displayHomepage() {
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		Scanner t = new Scanner(System.in);
+		SignUp signup = new SignUp();
+		Login login = new Login();
+
+		System.out.println("Enter choice (1-3)\n 1. Login\n 2. Sign Up\n 3. Exit");
+		int user_choice = t.nextInt();
+		switch (user_choice) {
+		case 1:
+			login.displayLogin();
+			break;
+		case 2:
+			signup.displaySignUp();
+			break;
+		case 3:
+			System.out.println("Thanks for visiting this portal....!!!!");
+			break;
+		default:
+			System.out.println("Please enter a valid choice");
+			displayHomepage();
+			break;
+		}
+	}
+}
